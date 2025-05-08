51d66b84f8b9f029ece8ddbe1c3c449120f281e7
feras <frzfrsfra3@gmail.com>
2025-05-08 12:19:21 +0300

    removed unnecessary files

--- lib/utils/date copy.tsx
+++
@@ -1,8 +0,0 @@
-// /src/lib/utils/date.ts
-export const isTodayVisible = (visibilityDays: string[]): boolean => {
-    const today = new Date().getDay(); // 0 (Sunday) to 6 (Saturday)
-    const dayNames = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-    const todayName = dayNames[today];
-    
-    return visibilityDays.includes(todayName);
-  };
